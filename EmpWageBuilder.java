8de57b3202c1e7354c0ec9d5691845a5f91c3c05
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-06 16:16:45 +0530

    Add: part time employee and daily wage calculation

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,22 +1,33 @@
 public class EmpWageBuilder {
 	
+
 	public static void main (String args[]) {
-		//Constant
+		System.out.println("Welcome to employee wage computation problem");
+
+		//Constants
 		int FULL_TIME = 1;
+		int PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 		int FULL_TIME_HOUR = 8;
-		
-		//Random number generation
-		double employeeCheck = Math.floor(Math.random() * 10) % 2;
+
+		//Variables
+		int workHours = 0;
+
+		double employeeCheck = Math.floor(Math.random()*10)%3;
 
 		//Attendance check
 		if(employeeCheck == FULL_TIME) {
-			System.out.println("Employee is present");
-			System.out.println("Employee daily wage is " +FULL_TIME_HOUR * WAGE_PER_HOUR);
+			workHours = 8;
+			System.out.println("Employee is present full time");
+		}
+		else if(employeeCheck == PART_TIME) {
+			workHours = 4;
+			System.out.println("Employee is present part time");
 		}
 		else {
+			workHours = 0;
 			System.out.println("Employee is absent");
 		}
+		System.out.println("Employee daily wage is " +workHours * WAGE_PER_HOUR);
 	}
-
 }
