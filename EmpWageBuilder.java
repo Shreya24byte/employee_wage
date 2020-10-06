6725a773289300d3f51cea2b51acad35a13e641d
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-06 19:00:06 +0530

    Refactor: manage employee wage of multiple companies

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,70 +1,98 @@
-public class EmpWageBuilder {
+package AssignmentProbs;
 
+class CompanyEmpWage {
+	//Constants
+	public final String company;
+	public final int wage_per_hour;
+	public final int max_working_days;
+	public final int max_working_hours;
+	public int totalWage;
+	
+	//Constructor
+	public CompanyEmpWage(String company,int wage_per_hour,int max_working_days,int max_working_hours ) {
+		this.company = company;
+		this.wage_per_hour = wage_per_hour;
+		this.max_working_days = max_working_days;
+		this.max_working_hours = max_working_days;
+	}
+	
+	public void setTotalEmpWage(int totalWage) {
+		this.totalWage = totalWage;
+	}
+	public String toString() {
+		return "Total employee wage for " +company+ " is " +totalWage;
+	}
+}
+
+public class EmpWageBuilder {
+	
 		//Constants
 		public static final int FULL_TIME = 1;
 		public static final int PART_TIME = 2;
 		
-		private final String company;
-		private final int wage_per_hour;
-		private final int max_working_days;
-		private final int max_working_hours;
-		private int totalWage;
+		private int numOfCompany = 0;
+		//Array
+		private CompanyEmpWage[] companyEmpWageArray;
 		
-		//Constructor
-		public EmpWageBuilder(String company,int wage_per_hour,int max_working_days,int max_working_hours ) {
-			this.company = company;
-			this.wage_per_hour = wage_per_hour;
-			this.max_working_days = max_working_days;
-			this.max_working_hours = max_working_hours;
+		public EmpWageBuilder() {
+			companyEmpWageArray = new CompanyEmpWage[5];
 		}
 		
-		//Class method
-		public void employeeWageComputation() {
-			System.out.println(company);
-			//Variables
-			int workHours = 0;
-			int dailyWage = 0;
-			int totalWorkingHours = 0;
-			int totalWorkingDays = 0;
-				
-			while(totalWorkingDays < max_working_days && totalWorkingHours < max_working_hours) {
-				totalWorkingDays++;
-					
-				int employeeCheck = (int) (Math.floor(Math.random()*10)%3);
-					
-				//Attendance check
-				switch(employeeCheck) {
-				case FULL_TIME:
-					workHours = 8;
-					break;
-				case PART_TIME:
-					workHours = 4;
-					break;
-				default:
-					workHours = 0;
-					break;
-				}
-			
-			//daily wage calculations
-			dailyWage = workHours * wage_per_hour;
-			totalWorkingHours += workHours;
-				
-			System.out.println("Day " +totalWorkingDays+ " employee work hours " +workHours+ " and daily wage is " +dailyWage);
+		private void addCompanyEmpWage(String company,int wage_per_hour,int max_working_days,int max_working_hours ) {
+			companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, wage_per_hour, max_working_days, max_working_hours );
+			numOfCompany++;
+		}
+		
+		private void employeeWageComputation() {
+			for (int i=0; i<numOfCompany; i++) {
+				companyEmpWageArray[i].setTotalEmpWage(this.employeeWageComputation(companyEmpWageArray[i]));
+				System.out.println(companyEmpWageArray[i]);
 			}
-			int totalWage = totalWorkingHours * wage_per_hour;
 		}
 		
-		public String toString() {
-			return "Total employee wage for " +company+ " is " +totalWage;
+		
+		private int employeeWageComputation(CompanyEmpWage companyEmpWage) {
+				//Variables
+				int workHours = 0;
+				int dailyWage = 0;
+				int totalWorkingHours = 0;
+				int totalWorkingDays = 0;
+				
+				//computation
+				while(totalWorkingDays <companyEmpWage.max_working_days && totalWorkingHours < companyEmpWage.max_working_hours) {
+					totalWorkingDays++;
+					
+					int employeeCheck = (int) (Math.floor(Math.random()*10)%3);
+					
+					//Attendance check
+					switch(employeeCheck) {
+					case FULL_TIME:
+						workHours = 8;
+						break;
+					case PART_TIME:
+						workHours = 4;
+						break;
+					default:
+						workHours = 0;
+						break;
+					}
+			
+				//daily wage calculations
+				dailyWage = workHours * companyEmpWage.wage_per_hour;
+				totalWorkingHours += workHours;
+				
+				System.out.println("Day " +totalWorkingDays+ " employee work hours " +workHours+ " and daily wage is " +dailyWage);
+				}
+				return totalWorkingHours * companyEmpWage.wage_per_hour;
 		}
+		
 		
 		public static void main (String args[]) {
 			System.out.println("Welcome to employee wage computation problem");
 			
-			EmpWageBuilder company1 = new EmpWageBuilder("Reliance", 20, 20, 100);
-			company1.employeeWageComputation();
-			
-			EmpWageBuilder company2 = new EmpWageBuilder("Idea", 10, 30, 100);
-			company2.employeeWageComputation();
+			EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+			empWageBuilder.addCompanyEmpWage("Reliance", 20, 20, 100);
+			empWageBuilder.addCompanyEmpWage("Idea", 10, 20, 150);
+			empWageBuilder.employeeWageComputation();
 		}	
 }
