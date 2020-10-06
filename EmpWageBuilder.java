8cbbcaf5eb6a28e6f823da3fa94616a799591ee6
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-06 17:18:34 +0530

    Add: employee wage computation for multiple companies

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,20 +1,20 @@
 public class EmpWageBuilder {
-	//Constants
-	public static final int FULL_TIME = 1;
-	public static final int PART_TIME = 2;
-	public static final int WAGE_PER_HOUR = 20;
-	public static final int MAX_WORKING_DAYS = 20;
-	public static final int MAX_WORKING_HOURS = 100; 
+
+		//Constants
+		public static final int FULL_TIME = 1;
+		public static final int PART_TIME = 2;
 		
-	public static int EmployeeWageComputation() {
-				
+		//Class method
+		void employeeWageComputation(String company, int wage_per_hour, int max_working_days, int max_working_hours) {
+				System.out.println(company+ " company employee wage computation");
 		//Variables
 		int workHours = 0;
 		int dailyWage = 0;
 		int totalWorkingHours = 0;
 		int totalWorkingDays = 0;
 				
-		while(totalWorkingDays < MAX_WORKING_DAYS && totalWorkingHours < MAX_WORKING_HOURS) {
+
+		  while(totalWorkingDays <= max_working_days && totalWorkingHours < max_working_hours) {
 			totalWorkingDays++;
 					
 			int employeeCheck = (int) (Math.floor(Math.random()*10)%3);
@@ -22,7 +22,7 @@ public class EmpWageBuilder {
 			//Attendance check
 			switch(employeeCheck) {
 			case FULL_TIME:
-				workHours = 8;				
+				workHours = 8;
 				break;
 			case PART_TIME:
 				workHours = 4;
@@ -33,17 +33,21 @@ public class EmpWageBuilder {
 			}
 			
 		//daily wage calculations
-		dailyWage = workHours * WAGE_PER_HOUR;
+		dailyWage = workHours * wage_per_hour;
 		totalWorkingHours += workHours;
 				
 		System.out.println("Day " +totalWorkingDays+ " employee work hours " +workHours+ " and daily wage is " +dailyWage);
 		}
-		System.out.println("Employee monthly wage is " +totalWorkingHours * WAGE_PER_HOUR);
-		return totalWorkingHours * WAGE_PER_HOUR;
-	}	
-
-	public static void main (String args[]) {
-		System.out.println("Welcome to employee wage computation problem");
-		EmployeeWageComputation();
+		System.out.println("Employee monthly wage is " +totalWorkingHours * wage_per_hour);
+		System.out.println("----------------------------------------------------------");
 	}
+		public static void main (String args[]) {
+			System.out.println("Welcome to employee wage computation problem");
+			
+			EmpWageBuilder company1 = new EmpWageBuilder();
+			company1.employeeWageComputation("Reliance", 20, 20, 100);
+			
+			EmpWageBuilder company2 = new EmpWageBuilder();
+			company2.employeeWageComputation("Idea", 10, 30, 100);
+		}
 }
