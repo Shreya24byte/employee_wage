95966e809fee4cdba1b8134c11e00873b3746fb8
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-06 16:11:37 +0530

    Add: full time employee daily wage calculation

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,6 +3,8 @@ public class EmpWageBuilder {
 	public static void main (String args[]) {
 		//Constant
 		int FULL_TIME = 1;
+		int WAGE_PER_HOUR = 20;
+		int FULL_TIME_HOUR = 8;
 		
 		//Random number generation
 		double employeeCheck = Math.floor(Math.random() * 10) % 2;
@@ -10,6 +12,7 @@ public class EmpWageBuilder {
 		//Attendance check
 		if(employeeCheck == FULL_TIME) {
 			System.out.println("Employee is present");
+			System.out.println("Employee daily wage is " +FULL_TIME_HOUR * WAGE_PER_HOUR);
 		}
 		else {
 			System.out.println("Employee is absent");
