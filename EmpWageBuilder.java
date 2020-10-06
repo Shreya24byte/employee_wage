07d527fcd617e0d3f2dfba90ae476b614e6f7642
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-06 16:45:11 +0530

    Add: wage calculation till conditions are satisfied

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,22 +3,24 @@ public class EmpWageBuilder {
 
 	public static void main (String args[]) {
 		System.out.println("Welcome to employee wage computation problem");
-
 		//Constants
 		final int FULL_TIME = 1;
 		final int PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
 		final int MAX_WORKING_DAYS = 20;
-		
+		final int MAX_WORKING_HOURS = 100;
+				
 		//Variables
 		int workHours = 0;
 		int dailyWage = 0;
 		int totalWorkingHours = 0;
-
-		for(int day = 1; day<=MAX_WORKING_DAYS; day++) {
-			
-			int employeeCheck = (int) (Math.floor(Math.random()*10)%3);
+		int totalWorkingDays = 0;
 				
+		while(totalWorkingDays < MAX_WORKING_DAYS && totalWorkingHours < MAX_WORKING_HOURS) {
+			totalWorkingDays++;
+					
+			int employeeCheck = (int) (Math.floor(Math.random()*10)%3);
+					
 			//Attendance check
 			switch(employeeCheck) {
 			case FULL_TIME:
@@ -31,13 +33,13 @@ public class EmpWageBuilder {
 				workHours = 0;
 				break;
 			}
-		
-		//daily wage calculation
+			
+		//daily wage calculations
 		dailyWage = workHours * WAGE_PER_HOUR;
 		totalWorkingHours += workHours;
-		
-		System.out.println("Employee work hours " +workHours+ " and daily wage is " +dailyWage);
+				
+		System.out.println("Day " +totalWorkingDays+ " employee work hours " +workHours+ " and daily wage is " +dailyWage);
 		}
-		System.out.println("Employee monthly wage is " +totalWorkingHours * WAGE_PER_HOUR);		
+		System.out.println("Employee monthly wage is " +totalWorkingHours * WAGE_PER_HOUR);
 	}
 }
