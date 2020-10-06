a1002cc3c9894438ee8057213097682d11b4029c
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-06 16:23:12 +0530

    Add: switch case statements

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,31 +3,32 @@ public class EmpWageBuilder {
 
 	public static void main (String args[]) {
 		System.out.println("Welcome to employee wage computation problem");
-
 		//Constants
-		int FULL_TIME = 1;
-		int PART_TIME = 2;
-		int WAGE_PER_HOUR = 20;
-		int FULL_TIME_HOUR = 8;
-
+		final int FULL_TIME = 1;
+		final int PART_TIME = 2;
+		final int WAGE_PER_HOUR = 20;
+		
 		//Variables
 		int workHours = 0;
-
-		double employeeCheck = Math.floor(Math.random()*10)%3;
-
+		
+		int employeeCheck = (int) (Math.floor(Math.random()*10)%3);
+		
 		//Attendance check
-		if(employeeCheck == FULL_TIME) {
+		switch(employeeCheck) {
+		case FULL_TIME:
 			workHours = 8;
 			System.out.println("Employee is present full time");
-		}
-		else if(employeeCheck == PART_TIME) {
+			break;
+		case PART_TIME:
 			workHours = 4;
 			System.out.println("Employee is present part time");
-		}
-		else {
+			break;
+		default:
 			workHours = 0;
 			System.out.println("Employee is absent");
+			break;
 		}
 		System.out.println("Employee daily wage is " +workHours * WAGE_PER_HOUR);
+	
 	}
 }
