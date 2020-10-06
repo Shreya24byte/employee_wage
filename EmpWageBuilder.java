d364d3e0d2d837adcb5d38535a3576fe3de15b5a
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-06 16:05:50 +0530

    Add: conditional statements to check employee attendance

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,19 @@
 public class EmpWageBuilder {
 	
 	public static void main (String args[]) {
-		System.out.println("Welcome to employee wage computation problem");
+		//Constant
+		int FULL_TIME = 1;
+		
+		//Random number generation
+		double employeeCheck = Math.floor(Math.random() * 10) % 2;
+
+		//Attendance check
+		if(employeeCheck == FULL_TIME) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
 	}
 
 }
